7995bc003dd63e468a8648127ffaa4ff0c50d9fb
Gabriel Oquendo <oquendogabriel18@gmail.com>
2024-10-27 22:50:15 -0500

    Add employee and parking controllers with create, login, get, and close functionality

---
+++ src/controllers/employee.controllers.js
@@ -0,0 +1,60 @@
+import Employee from "../models/employee.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config.js";
+
+export const createEmployee = async (req, res) => {
+  const { name, email, password } = req.body;
+  const employeeFound = await Employee.findOne({
+    emai: email,
+  });
+
+  if (employeeFound)
+    return res.status(400).json({ message: "The email already exists" });
+  const newEmployee = new Employee({ name, email, password });
+  try {
+    newEmployee.password = await bcrypt.hash(password, 10);
+    const EmployeeSaved = await newEmployee.save();
+    return res.send(EmployeeSaved);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const loginEmployee = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const employee = await Employee.findOne({ email });
+
+    if (!employee)
+      return res.status(400).json({ message: "Usuario no encontrado" });
+    const matchPassword = await bcrypt.compare(password, employee.password);
+
+    if (!matchPassword)
+      return res
+        .status(401)
+        .json({ token: null, message: "Contraseña invalida" });
+
+    const token = jwt.sign(
+      { _id: employee._id, email: employee.email, name: employee.name },
+      SECRET_KEY,
+      {
+        expiresIn: 86400,
+      }
+    );
+    return res.json({
+      token,
+      employee: {
+        name: employee.name,
+        email: employee.email,
+        _id: employee._id,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "A ocurrido un error inesperado" });
+  }
+};
+
+export const getEmployees = async (req, res) => {
+  return res.json({ employee: req.user });
+};
---
+++ src/controllers/parking.controllers.js
@@ -0,0 +1,58 @@
+import Parking from "../models/parking.model.js";
+
+export const newRegister = async (req, res) => {
+  try {
+    const { plate, checkIn, checkOut, nameVehicle, owner, documentNumber } =
+      req.body;
+
+    const employee = req.user._id;
+    const newParking = new Parking({
+      plate,
+      checkIn,
+      checkOut,
+      nameVehicle,
+      owner,
+      documentNumber,
+      employee,
+    });
+    if (!newParking)
+      return res
+        .status(400)
+        .json({ message: "No se ha podido registrar el vehículo" });
+    const parkingSaved = await newParking.save();
+    console.log(parkingSaved);
+    return res.status(201).json(parkingSaved);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "A ocurrido un error inesperado" });
+  }
+};
+
+export const getParkings = async (req, res) => {
+  try {
+    const parkings = await Parking.find({
+      employee: req.user._id,
+    });
+    const filterParkings = parkings.filter(
+      (parking) => parking.status === "ACTIVE"
+    );
+    return res.json(filterParkings);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "A ocurrido un error inesperado" });
+  }
+};
+
+export const closeRegister = async (req, res) => {
+  try {
+    const parking = await Parking.findById(req.params.id);
+    if (!parking)
+      return res.status(404).json({ message: "Registro no encontrado" });
+    parking.checkOut = new Date();
+    parking.status = "COMPLETED";
+    const parkingUpdated = await parking.save();
+    return res.json(parkingUpdated);
+  } catch (error) {
+    return res.status(400).json({ message: "A ocurrido un error inesperado" });
+  }
+};
